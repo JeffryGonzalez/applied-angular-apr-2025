e02bba051b57580c8766d36ae670d72a7096b134
Jeffry Gonzalez <jeff@hypertheory.com>
2025-04-28 20:44:36 +0000

    done with day 1

--- src/demos/pages/signal-demos.ts
+++ src/demos/pages/signal-demos.ts
@@ -1,10 +1,39 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  signal,
+  effect,
+  computed,
+} from '@angular/core';
 
 @Component({
   selector: 'app-demos-signals',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
-  template: ` <p>Signals Demos Coming Soon</p> `,
+  template: `
+    <p>Signals Demos Coming Soon</p>
+    <p>{{ message() }}</p>
+
+    <p>There are {{ numberOfLettersInMessage() }} letters in that.</p>
+    <button (click)="embiggenate()" class="btn btn-primary">Embiggenate</button>
+    <button class="btn btn-warning">Does Nothing</button>
+  `,
   styles: ``,
 })
-export class SignalDemosComponent {}
+export class SignalDemosComponent {
+  // never ever use "raw" data like this again.
+  message = signal('This is the original message');
+
+  numberOfLettersInMessage = computed(() => this.message().length);
+
+  embiggenate() {
+    // this.message.set(this.message().toUpperCase());
+    this.message.update((old) => old.toUpperCase());
+  }
+
+  constructor() {
+    effect(() => {
+      setInterval(() => this.message.update((m) => m + 'x'), 1000);
+    });
+  }
+}
