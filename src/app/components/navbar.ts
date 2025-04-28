eb9bef0afe464821dee597aeb43392475857f5ea
Jeffry Gonzalez <jeff@hypertheory.com>
2025-04-28 16:30:15 +0000

    before lunch

--- src/app/components/navbar.ts
+++ src/app/components/navbar.ts
@@ -28,7 +28,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
             tabindex="0"
             class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
-            <li><a>Item 1</a></li>
+            <li><a>Developer News</a></li>
             <li>
               <a>Parent</a>
               <ul class="p-2">
@@ -46,7 +46,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
           <li><a>Item 1</a></li>
           <li>
             <details>
-              <summary>Parent</summary>
+              <summary>Developer News</summary>
               <ul class="p-2">
                 <li><a>Submenu 1</a></li>
                 <li><a>Submenu 2</a></li>
