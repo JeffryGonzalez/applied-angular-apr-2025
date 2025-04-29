085588e2869af8a36c9869ddf90e337faabfb0a8
Jeffry Gonzalez <jeff@hypertheory.com>
2025-04-29 14:11:31 -0400

    ratings

--- src/developer-news/components/news-rating.ts
+++ src/developer-news/components/news-rating.ts
@@ -18,37 +18,15 @@ import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
 
       @if (linkRead()) {
         <div class="rating">
-          <input
-            type="radio"
-            name="rating-4"
-            class="mask mask-star-2 bg-green-500"
-            aria-label="1 star"
-          />
-          <input
-            type="radio"
-            name="rating-4"
-            class="mask mask-star-2 bg-green-500"
-            aria-label="2 star"
-            checked="checked"
-          />
-          <input
-            type="radio"
-            name="rating-4"
-            class="mask mask-star-2 bg-green-500"
-            aria-label="3 star"
-          />
-          <input
-            type="radio"
-            name="rating-4"
-            class="mask mask-star-2 bg-green-500"
-            aria-label="4 star"
-          />
-          <input
-            type="radio"
-            name="rating-4"
-            class="mask mask-star-2 bg-green-500"
-            aria-label="5 star"
-          />
+          @for (rating of linkRatings; track rating) {
+            <input
+              type="radio"
+              (click)="linkRating.set(rating)"
+              [name]="'rating-' + rating"
+              class="mask mask-star-2 bg-green-500"
+              [attr.aria-label]="rating + ' star'"
+            />
+          }
         </div>
       }
 
@@ -60,6 +38,8 @@ import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
 export class NewsRatingComponent {
   linkRead = signal(false);
 
+  linkRating = signal<1 | 2 | 3 | 4 | 5>(1);
+  linkRatings = [1, 2, 3, 4, 5] as const;
   toggleLinkRead() {
     this.linkRead.update((r) => !r);
   }
