94a50996dc77a0342e30930a6568b99d9ab38a7b
Jeffry Gonzalez <jeff@hypertheory.com>
2025-04-29 15:48:56 +0000

    list prototype

--- src/developer-news/pages/list.ts
+++ src/developer-news/pages/list.ts
@@ -1,10 +1,45 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
+import { NewLinkModel } from '../types';
 
 @Component({
   selector: 'app-developer-news-list',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
-  template: ` <p>List Will Go Here</p> `,
+  template: `
+    <ul>
+      @for (link of links(); track link.id) {
+        <li class="card card-border bg-base-100 w-96">
+          <div class="card-body">
+            <p class="card-title text-accent">
+              {{ link.title }}
+            </p>
+            <a [href]="link.href" class="link" target="_blank">{{
+              link.href
+            }}</a>
+            @if (link.description) {
+              <p>
+                {{ link.description }}
+              </p>
+            }
+          </div>
+        </li>
+      }
+    </ul>
+  `,
   styles: ``,
 })
-export class ListComponent {}
+export class ListComponent {
+  links = signal<NewLinkModel[]>([
+    {
+      id: '1',
+      title: 'Applied Angular Course Material',
+      description: 'Stuff from class',
+      href: 'https://applied-angular.hypertheory.com',
+    },
+    {
+      id: '99',
+      title: 'TypeScript Official Documentation',
+      href: 'https://typescriptlang.org',
+    },
+  ]);
+}
---
+++ src/developer-news/types.ts
@@ -0,0 +1,6 @@
+export type NewLinkModel = {
+  id: string;
+  title: string;
+  description?: string;
+  href: string;
+};
