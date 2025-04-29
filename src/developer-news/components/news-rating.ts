0d4c8a42d306e5951bfd79ee2e315acdb73f400f
Jeffry Gonzalez <jeff@hypertheory.com>
2025-04-29 14:03:08 -0400

    can read links

--- src/developer-news/components/news-rating.ts
+++ src/developer-news/components/news-rating.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
 
 @Component({
   selector: 'app-news-rating',
@@ -7,47 +7,60 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   template: `
     <div class="flex gap-2">
       <label class="label">
-        <input type="checkbox" checked="checked" class="toggle" />
+        <input
+          type="checkbox"
+          [checked]="linkRead()"
+          (click)="toggleLinkRead()"
+          class="toggle"
+        />
         Link Read
       </label>
 
-      <div class="rating">
-        <input
-          type="radio"
-          name="rating-4"
-          class="mask mask-star-2 bg-green-500"
-          aria-label="1 star"
-        />
-        <input
-          type="radio"
-          name="rating-4"
-          class="mask mask-star-2 bg-green-500"
-          aria-label="2 star"
-          checked="checked"
-        />
-        <input
-          type="radio"
-          name="rating-4"
-          class="mask mask-star-2 bg-green-500"
-          aria-label="3 star"
-        />
-        <input
-          type="radio"
-          name="rating-4"
-          class="mask mask-star-2 bg-green-500"
-          aria-label="4 star"
-        />
-        <input
-          type="radio"
-          name="rating-4"
-          class="mask mask-star-2 bg-green-500"
-          aria-label="5 star"
-        />
-      </div>
+      @if (linkRead()) {
+        <div class="rating">
+          <input
+            type="radio"
+            name="rating-4"
+            class="mask mask-star-2 bg-green-500"
+            aria-label="1 star"
+          />
+          <input
+            type="radio"
+            name="rating-4"
+            class="mask mask-star-2 bg-green-500"
+            aria-label="2 star"
+            checked="checked"
+          />
+          <input
+            type="radio"
+            name="rating-4"
+            class="mask mask-star-2 bg-green-500"
+            aria-label="3 star"
+          />
+          <input
+            type="radio"
+            name="rating-4"
+            class="mask mask-star-2 bg-green-500"
+            aria-label="4 star"
+          />
+          <input
+            type="radio"
+            name="rating-4"
+            class="mask mask-star-2 bg-green-500"
+            aria-label="5 star"
+          />
+        </div>
+      }
 
       <button class="btn btn-circle btn-error">X</button>
     </div>
   `,
   styles: ``,
 })
-export class NewsRatingComponent {}
+export class NewsRatingComponent {
+  linkRead = signal(false);
+
+  toggleLinkRead() {
+    this.linkRead.update((r) => !r);
+  }
+}
